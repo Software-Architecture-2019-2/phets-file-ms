3887a9f033ed8e82b91f470cea4d7c134d804458
Julian Salomon <jesalomont@unal.edu.co>
2019-10-23 19:56:06 -0500

    + Cors

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,15 @@
 import express from "express";
+import cors from "cors";
 import * as config from "./config";
-import {FileResource} from "./resource/file_resource";
+import { FileResource } from "./resource/file_resource";
 
 // Create a new express application instance
 const app = express();
+app.use(cors());
 
 FileResource.setupResources(app);
 app.listen(config.PORT, () => {
-    console.log(`Listening on port ${config.PORT}!`);
+  console.log(`Listening on port ${config.PORT}!`);
 });
 
 export default app;
